1476f7fc8e46fb6029ecd95b8f4d43b9ac313e54
Joaquim <joamartins077@gmail.com>
2024-12-07 15:13:59 +0530

    cmd06122024

--- app/src/main/java/in/megasoft/workplace/ui/main/PlaceholderFragment.java
+++ app/src/main/java/in/megasoft/workplace/ui/main/PlaceholderFragment.java
@@ -6,15 +6,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import in.megasoft.workplace.R;
-
-import `in`.megasoft.workplace.databinding.FragmentAttendanceDetailsBinding;
+import in.megasoft.workplace.databinding.FragmentAttendanceDetailsBinding;
 
 /**
  * A placeholder fragment containing a simple view.
