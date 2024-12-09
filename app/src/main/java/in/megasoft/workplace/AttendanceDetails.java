7dfcd187d1c3f68534a83ed9eb6614748eb43083
Joaquim <joamartins077@gmail.com>
2024-12-09 16:08:40 +0530

    cmd09122024

--- app/src/main/java/in/megasoft/workplace/AttendanceDetails.java
+++ app/src/main/java/in/megasoft/workplace/AttendanceDetails.java
@@ -4,9 +4,10 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class AttendanceDetails extends AppCompatActivity {
 
@@ -15,10 +16,23 @@ public class AttendanceDetails extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_attendance_details);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        ViewPager2 viewPager = findViewById(R.id.view_pager);
+        TabLayout tabs = findViewById(R.id.tabs);
+
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this);
+        viewPager.setAdapter(sectionsPagerAdapter);
+
+        new TabLayoutMediator(tabs, viewPager, (tab, position) -> {
+            switch (position) {
+                case 0:
+                    tab.setText("Month");
+                    break;
+                case 1:
+                    tab.setText("Employee");
+                    break;
+            }
+        }).attach();
     }
+
+
 }
---
+++ app/src/main/java/in/megasoft/workplace/SectionsPagerAdapter.java
@@ -0,0 +1,31 @@
+package in.megasoft.workplace;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class SectionsPagerAdapter extends FragmentStateAdapter {
+
+    public SectionsPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return new Tab1Fragment();
+            case 1:
+                return new Tab2Fragment();
+            default:
+                throw new IllegalArgumentException("Invalid position");
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2; // Number of tabs
+    }
+}
