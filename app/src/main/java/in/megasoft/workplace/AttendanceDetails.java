70576ee4b300a988756a1c6879f8fc9553acaf64
Joaquim <joamartins077@gmail.com>
2024-12-10 15:27:06 +0530

    cmd12122024

--- app/src/main/java/in/megasoft/workplace/AttendanceDetails.java
+++ app/src/main/java/in/megasoft/workplace/AttendanceDetails.java
@@ -3,10 +3,6 @@ package in.megasoft.workplace;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager2.widget.ViewPager2;
-
-import com.google.android.material.tabs.TabLayout;
-import com.google.android.material.tabs.TabLayoutMediator;
 
 public class AttendanceDetails extends AppCompatActivity {
 
@@ -14,21 +10,8 @@ public class AttendanceDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_attendance_details);
-        ViewPager2 viewPager = findViewById(R.id.view_pager);
-        TabLayout tabs = findViewById(R.id.tabs);
 
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this);
-        viewPager.setAdapter(sectionsPagerAdapter);
 
-        new TabLayoutMediator(tabs, viewPager, (tab, position) -> {
-            switch (position) {
-                case 0:
-                    tab.setText("Month");
-                    break;
-                case 1:
-                    tab.setText("Employee");
-                    break;
-            }
-        }).attach();
+
     }
 }
