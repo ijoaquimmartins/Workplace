d7dca9f6dd7a33ec606e86cae32035000acf06c1
Joaquim <joamartins077@gmail.com>
2024-12-21 11:39:12 +0530

    cmd18122024

--- app/src/main/java/in/megasoft/workplace/EmployeeDetails.java
+++ app/src/main/java/in/megasoft/workplace/EmployeeDetails.java
@@ -16,10 +16,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.Response.ErrorListener;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
@@ -34,6 +38,8 @@ public class EmployeeDetails extends AppCompatActivity {
     ArrayList<String> usernames = new ArrayList<>();
     final HashMap<String, String> userMap = new HashMap<>();
     private RequestQueue requestQueue;
+
+    String selectedUserid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -97,7 +103,9 @@ public class EmployeeDetails extends AppCompatActivity {
                         }
                     }
                     // Populate spinner
+                    String select = "Select Employee";
                     ArrayList<String> userDisplayList = new ArrayList<>();
+                    userDisplayList.add(0, select);
                     for (int i = 0; i < employeeIds.size(); i++) {
                         userDisplayList.add(employeeIds.get(i) + " - " + employeeNames.get(i));
                     }
@@ -110,10 +118,14 @@ public class EmployeeDetails extends AppCompatActivity {
                         public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                             String selectedItem = parent.getItemAtPosition(position).toString();
                             String[] splitItem = selectedItem.split(" - ");
-                            if (splitItem.length == 2) {
-                                String selectedUserid = splitItem[0];
-                                String selectedUsername = splitItem[1];
-                                Toast.makeText(EmployeeDetails.this, "UserID: " + selectedUserid + "\nUsername: " + selectedUsername, Toast.LENGTH_SHORT).show();
+
+                            if(!selectedItem.equals("Select Employee")){
+                                if (splitItem.length == 2) {
+                                    selectedUserid = splitItem[0];
+                                    String selectedUsername = splitItem[1];
+                                    getEmployeeDetails();
+                                //    Toast.makeText(EmployeeDetails.this, "UserID: " + selectedUserid + "\nUsername: " + selectedUsername, Toast.LENGTH_SHORT).show();
+                                }
                             }
                         }
                         @Override
@@ -124,7 +136,7 @@ public class EmployeeDetails extends AppCompatActivity {
 
                 }
             },
-            new Response.ErrorListener() {
+            new ErrorListener() {
                 @Override
                 public void onErrorResponse(VolleyError error) {
                     Log.e("Volley Error", error.toString());
@@ -135,29 +147,46 @@ public class EmployeeDetails extends AppCompatActivity {
         requestQueue.add(jsonArrayRequest);
     }
     public void getEmployeeDetails(){
-        String urlFetch = PublicURL + "getemployeelist.php";
+        String urlFetch = PublicURL + "employeedetails.php?id=" + selectedUserid;
+        RequestQueue request = Volley.newRequestQueue(this);
         HttpsTrustManager.allowAllSSL();
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, urlFetch, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONArray jsonArray = new JSONArray(response);
 
-        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, urlFetch, null,
-                new Response.Listener<JSONArray>() {
-                    @Override
-                    public void onResponse(JSONArray response) {
-                        if (response == null || response.length() == 0) {
-                            Toast.makeText(EmployeeDetails.this, "NO DATA", Toast.LENGTH_LONG).show();
-                            return;
-                        }else {
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        JSONObject jsonObject = jsonArray.getJSONObject(i);
 
-                        }
+                        txtUserFullName.setText(jsonObject.getString("username"));
+                        txtUserCode.setText(jsonObject.getString("empcode"));
+                        txtMobile.setText(jsonObject.getString("phone"));
+                        txtMobileAlt.setText(jsonObject.getString("altphone"));
+                        txtEmailID.setText(jsonObject.getString("email"));
+                        txtDesignation.setText(jsonObject.getString("designation"));
+                        txtLocation.setText(jsonObject.getString("elocation"));
+                        txtEmployeeType.setText(jsonObject.getString("emptype"));
+                        txtDOB.setText(jsonObject.getString("dobirth"));
+                        txtDOJ.setText(jsonObject.getString("dojoin"));
+                        txtLeavesAllotted.setText(jsonObject.getString("totalleaves"));
+                        txtPresent.setText(jsonObject.getString("present"));
+                        txtAbsent.setText(jsonObject.getString("absent"));
+                        txtLeavesTaken.setText(jsonObject.getString("leavestaken"));
+                        txtBalanceLeaves.setText(jsonObject.getString("balanceleaves"));
+                        txtEL.setText(jsonObject.getString("earnedl"));
                     }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        Log.e("Volley Error", error.toString());
-                        Toast.makeText(EmployeeDetails.this, "Error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
                 }
-        );
-        requestQueue.add(jsonArrayRequest);
+            }
+        }, new ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        request.add(stringRequest);
+
+        }
     }
-}
